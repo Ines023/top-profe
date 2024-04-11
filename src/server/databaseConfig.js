6311a992a7ce5b06b0e36053ea0a1c63c4766ee3
Pablofl01 <pablofl01@gmail.com>
2024-04-11 22:16:51 +0200

    Updated dialect

--- src/server/databaseConfig.js
+++ src/server/databaseConfig.js
@@ -4,7 +4,7 @@ module.exports = {
 	database: config.database.dbName,
 	username: config.database.user,
 	password: config.database.password,
-	dialect: 'mysql',
+	dialect: 'mariadb',
 	host: config.database.host,
 	port: config.database.port,
 };
