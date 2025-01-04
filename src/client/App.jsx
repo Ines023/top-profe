8d7e01f74eb7612e5c2c9244ce338e2e014da89a
Inés García Ruiz <inesgar2004@gmail.com>
2025-01-04 21:41:15 +0100

    📦 NEW: App views updated to new possible views

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -9,8 +9,7 @@ import InitialMenu from './views/InitialMenu';
 import LoginErrorView from './views/LoginErrorView';
 import ProfessorList from './views/ProfessorList';
 import ProfessorProfile from './views/ProfessorProfile';
-import CompletedRankingView from './views/CompletedRankingView';
-import ProvisionalRankingView from './views/ProvisionalRankingView';
+import RankingView from './views/RankingView';
 import AdminMainView from './views/admin/AdminMainView';
 import AdminDegreesView from './views/admin/AdminDegreesView';
 import SubjectDetails from './views/SubjectDetails';
@@ -32,7 +31,7 @@ export default function App() {
 				<Route path="/" element={<InitialMenu />} />
 				<Route path="subjects/*" element={<SubjectRoutes />} />
 				<Route path="professors/*" element={<ProfessorRoutes />} />
-				<Route path="rankings/*" element={<RankingsRoutes />} />
+				<Route path="rankings/*" element={<RankingView />} />
 				<Route path="admin/*" element={<AdminRoutes />} />
 				<Route path="votes/:voteId" element={<VoteView />} />
 				<Route path="failed-login" element={<LoginErrorView />} />
@@ -84,14 +83,5 @@ function AdminRoutes() {
 				<Route path="users" element={<AdminEditUsersView />} />
 			</Routes>
 		</>
-	);
-}
-
-function RankingsRoutes() {
-	return (
-		<Routes>
-			<Route path="provisional" element={<ProvisionalRankingView />} />
-			<Route path="completed/:academicYear" element={<CompletedRankingView />} />
-		</Routes>
 	);
 }
--- src/client/views/RankingView.jsx
+++ src/client/views/RankingView.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { fetchGet } from '../util';
 
-export default class ProfessorList extends Component {
+export default class RankingView extends Component {
 	constructor() {
 		super();
 		this.state = {
 			isLoaded: false,
 			topProfessors: [],
-			botProfessors: [],
+			worstProfessors: [],
 			mostVotedProfessors: [],
 		};
 	}
@@ -18,8 +18,8 @@ export default class ProfessorList extends Component {
 			.then((res) => {
 				this.setState({
 					isLoaded: true,
-					topProfessors: res.topProfessors,
-					botProfessors: res.botProfessors,
+					topProfessors: res.topProfessors || [],
+					worstProfessors: res.worstProfessors || [],
 					mostVotedProfessors: res.mostVotedProfessors,
 				});
 			});
@@ -27,7 +27,7 @@ export default class ProfessorList extends Component {
 
 	render() {
 		const {
-			isLoaded, topProfessors, botProfessors, mostVotedProfessors,
+			isLoaded, topProfessors, worstProfessors, mostVotedProfessors,
 		} = this.state;
 
 		if (!isLoaded) return (<div className="full-width">Cargando...</div>);
@@ -35,68 +35,79 @@ export default class ProfessorList extends Component {
 		return (
 			<div>
 				<h2 className="centered">Ranking global</h2>
-				<p>
-					Estos son los tres principales rankings de todos los
-					profesores del Top Profe. Puedes entrar en el perfil de
-					cada profesor pulsando en su nombre.
-				</p>
-				<p>
-					Puedes encontrar más información sobre la
-					puntuación <em>TopScore</em> y cómo se calcula en
-					las <a href="https://dat.etsit.upm.es/top-profe#top-score">preguntas frecuentes</a>.
-				</p>
 
-				<h3>Los 10 mejores</h3>
-				<table className="full-width box">
-					<thead>
-						<tr>
-							<th>Nombre</th>
-							<th>TopScore</th>
-							<th>Media</th>
-						</tr>
-					</thead>
-					<tbody>
-						{ topProfessors.map(professor => (
-							<tr key={professor.id}>
-								<td>
-									<a href={`/professors/${professor.id}`}>
-										{professor.name}
-									</a>
-								</td>
-								<td>{professor.score.toFixed(1)}</td>
-								<td>
-									{professor.avg ? `${professor.avg.toFixed(2)}/5 (total: ${professor.count})` : '-'}
-								</td>
-							</tr>
-						)) }
-					</tbody>
-				</table>
+				{(topProfessors.length === 0 && worstProfessors.length === 0) ? (
+					<p>
+						Este es el ranking provisional del curso {config.server.currentAcademicYear} de todos los
+						profesores del Top Profe. Puedes entrar en el perfil de
+						cada profesor pulsando en su nombre.
+					</p>
+				) : (
+					<>
+						<p>
+							Estos son los tres principales rankings de todos los
+							profesores del Top Profe. Puedes entrar en el perfil de
+							cada profesor pulsando en su nombre.
+						</p>
+						<p>
+							Puedes encontrar más información sobre la
+							puntuación <em>TopScore</em> y cómo se calcula en
+							las <a href="https://dat.etsit.upm.es/top-profe#top-score">preguntas frecuentes</a>.
+						</p>
 
-				<h3>Los 10 peores</h3>
-				<table className="full-width box">
-					<thead>
-						<tr>
-							<th>Nombre</th>
-							<th>TopScore</th>
-							<th>Punt. media</th>
-						</tr>
-					</thead>
-					<tbody>
-						{ botProfessors.map(professor => (
-							<tr key={professor.id}>
-								<td>
-									<a href={`/professors/${professor.id}`}>
-										{professor.name}
-									</a>
-								</td>
-								<td>{professor.score.toFixed(1)}</td>
-								<td>
-									{professor.avg ? `${professor.avg.toFixed(2)}/5 (total: ${professor.count})` : '-'}
-								</td>
-							</tr>
-						)) }
-					</tbody>
-				</table>
+						<h3>Los 10 mejores</h3>
+						<table className="full-width box">
+							<thead>
+								<tr>
+									<th>Nombre</th>
+									<th>TopScore</th>
+									<th>Media</th>
+								</tr>
+							</thead>
+							<tbody>
+								{topProfessors.map(professor => (
+									<tr key={professor.id}>
+										<td>
+											<a href={`/professors/${professor.id}`}>
+												{professor.name}
+											</a>
+										</td>
+										<td>{professor.score.toFixed(1)}</td>
+										<td>
+											{professor.avg ? `${professor.avg.toFixed(2)}/5 (total: ${professor.count})` : '-'}
+										</td>
+									</tr>
+								))}
+							</tbody>
+						</table>
+
+						<h3>Los 10 peores</h3>
+						<table className="full-width box">
+							<thead>
+								<tr>
+									<th>Nombre</th>
+									<th>TopScore</th>
+									<th>Punt. media</th>
+								</tr>
+							</thead>
+							<tbody>
+								{worstProfessors.map(professor => (
+									<tr key={professor.id}>
+										<td>
+											<a href={`/professors/${professor.id}`}>
+												{professor.name}
+											</a>
+										</td>
+										<td>{professor.score.toFixed(1)}</td>
+										<td>
+											{professor.avg ? `${professor.avg.toFixed(2)}/5 (total: ${professor.count})` : '-'}
+										</td>
+									</tr>
+								))}
+							</tbody>
+						</table>
+					</>
+				)}
 
 				<h3>Los 10 más votados</h3>
 				<table className="full-width box">
@@ -107,7 +118,7 @@ export default class ProfessorList extends Component {
 						</tr>
 					</thead>
 					<tbody>
-						{ mostVotedProfessors.map(professor => (
+						{mostVotedProfessors.map(professor => (
 							<tr key={professor.id}>
 								<td>
 									<a href={`/professors/${professor.id}`}>
@@ -118,7 +129,7 @@ export default class ProfessorList extends Component {
 									{professor.avg ? `${professor.avg.toFixed(2)}/5 (total: ${professor.count})` : '-'}
 								</td>
 							</tr>
-						)) }
+						))}
 					</tbody>
 				</table>
 			</div>
